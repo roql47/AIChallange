a50dc645c54f2169af4d3f8cf5796879ddcb7d9d
roql47 <89699671+roql47@users.noreply.github.com>
2024-10-20 20:59:07 +0900

    service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,41 +1,19 @@
-const CACHE_NAME = 'v1';  // 캐시 이름
+const CACHE_NAME = 'v1';
 const CACHE_ASSETS = [
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/a_pacemaker_rhythm.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/accelerated_idioventricular_rhythm.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/anterior_fascicular_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/anterior_infarction_(lad).png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/aortic_dissection.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/asystole.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/atrial_fibrillation.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/atrial_flutter.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/atrial_premature_contraction.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/avrt.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/brugada_syndrome.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/complete_degree_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/early_repolarization_syndrome.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/first_degree_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/hyperkalemia.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/inferior_infarction_(rca).png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/junctional_escape_rhythm.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/left_bundle_branch_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/left_ventricle_hypertrophy.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/long_qt_syndrome.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/old_mi.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/posterior_fascicular_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/right_bundle_branch_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/second_degree_block_(mobitz_type_i).png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/second_degree_block_(mobitz_type_ii).png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/septic_shock.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/sinus_bradycardia.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/sinus_rhythm.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/sinus_tachycardia.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/stress_induced_cmp.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/svt.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/trifascicular_block.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/ventricular_fibrillation.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/ventricular_premature_contraction.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/ventricular_tachycardia.png',
-  'https://raw.githubusercontent.com/roql47/AIChallange/master/assets/assets/images/class/wellens_syndrome.png'
+  '/assets/assets/images/class/a_pacemaker_rhythm.png',
+  '/assets/assets/images/class/accelerated_idioventricular_rhythm.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_ASSETS)
+        .then(() => console.log('Files cached successfully'))
+        .catch((error) => console.error('Failed to cache some files:', error));
+    })
+  );
+});
+
   // 여기에 다른 URL들도 추가...
 ];
 
