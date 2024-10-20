d9b31edb5d508075214e01c19b9c52fda9f7bc1a
roql47 <89699671+roql47@users.noreply.github.com>
2024-10-20 21:27:29 +0900

    service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = 'v1';
+const CACHE_NAME = 'v1';  // 캐시 이름
 const CACHE_ASSETS = [
   '/assets/assets/images/class/a_pacemaker_rhythm.png',
   '/assets/assets/images/class/accelerated_idioventricular_rhythm.png',
-  // 여기에 다른 파일 경로도 추가할 수 있습니다
+  // 다른 파일 경로들도 여기에 추가하세요.
 ];
 
 // 설치 이벤트: 캐시할 파일들을 저장
 self.addEventListener('install', (event) => {
+  self.skipWaiting();  // 새로운 Service Worker를 바로 활성화
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
@@ -17,7 +18,23 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// 요청에 대한 응답을 캐시에서 제공
+// 활성화 이벤트: 이전 캐시를 삭제하고 새로운 캐시를 사용
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch 이벤트: 캐시된 파일이 있으면 제공하고, 없으면 네트워크에서 가져옴
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
